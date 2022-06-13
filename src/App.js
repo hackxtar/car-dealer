f9ac715d590d5276e148dfc315863d1ae3042859
Saif Ali <hackxtar@gmail.com>
2022-06-14 01:44:02 +0530

    Home grid Banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import SignIn from "./pages/components/auth/login/Signin";
-import Home from "./pages/components/Home";
+import Home from "./pages/components/home/Home";
 import Navbar from "./pages/components/shared/navbar/Navbar";
 import Footer from "./pages/components/shared/footer/Footer";
 
